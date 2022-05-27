ef518b80a31c12172086e24ab4a3d04404e79e0f
jeremyytann <tanchiachun2021@gmail.com>
2022-05-27 21:18:18 +0800

    fix enter created room but not directed

--- Code/frontend/src/Components/RoomCreateBody.js
+++ Code/frontend/src/Components/RoomCreateBody.js
@@ -76,92 +76,90 @@ const RoomCreateBody = () => {
 
     return (
         <Box height={580} mt={5} ml={10} mr={8} borderRadius={10} sx={{backgroundColor: 'white'}}>
-            <form onSubmit={createRoom}>
-                <Box mt={5} ml={5}>
-                    <Box pt={5} ml={1} display='flex' fontWeight='bold' fontSize={35}>
-                        创建房间
+            <Box mt={5} ml={5}>
+                <Box pt={5} ml={1} display='flex' fontWeight='bold' fontSize={35}>
+                    创建房间
+                </Box>
+
+                <Box display='flex' alignItems='center' pt={5}>
+                    <Box fontSize={25} fontWeight='bold' ml={1} width={125}>
+                        房间名
                     </Box>
 
-                    <Box display='flex' alignItems='center' pt={5}>
-                        <Box fontSize={25} fontWeight='bold' ml={1} width={125}>
-                            房间名
-                        </Box>
-
-                        <Box ml={5} display='flex' alignItems='center'>
-                            <Box fontSize={18}>
-                                <input 
-                                    className='create-room-name'
-                                    value={name}
-                                    onChange={event => setName(event.target.value)}
-                                    type='text'
-                                    placeholder='输入房间名' 
-                                    maxLength='30' required/>
-                            </Box>
-                        </Box>
-                    </Box>
-
-                    <Box display='flex' alignItems='center' mt={3}>
-                        <Box fontSize={25} fontWeight='bold' ml={1} width={125}>
-                            设置密码
-                        </Box>
-
-                        <Box ml={5} display='flex' alignItems='center'>
-                            <Box fontSize={18}>
-                                <Checkbox 
-                                    checked = {lock}
-                                    onChange={handleLockChange}
-                                    sx={{ '& .MuiSvgIcon-root': { fontSize: 40 } }}
-                                    color='default'
-                                />
-                            </Box>
-                        </Box>
-                    </Box>
-
-                    <Box height={170} mt={3}>
-                        { lock === true ? 
-                            <Box display='flex' alignItems='center'>
-                                <Box fontSize={25} fontWeight='bold' ml={1} width={125}>
-                                    密码
-                                </Box>
-
-                                <Box ml={5} display='flex' alignItems='center'>
-                                    <Box fontSize={18}>
-                                        <input 
-                                            className='create-room-name'
-                                            value={password}
-                                            onChange={event => setPassword(event.target.value)}
-                                            type={ visible ? 'text' : 'password' }
-                                            placeholder='输入密码' 
-                                            maxLength='30' required/>
-                                    </Box>
-                                </Box>
-
-                                <Box ml={4}>
-                                    { visible ? <VisibilityIcon sx={{cursor: 'pointer'}} onClick={() => setVisible(false)}/> : <VisibilityOffIcon sx={{cursor: 'pointer'}} onClick={() => setVisible(true)}/> }
-                                </Box>
-                            </Box> : ''
-                        }
-                    </Box>
-
-                    <Box mt={4} ml={2} display='flex'>
-                        <Box>
-                            <ThemeProvider theme={theme}>
-                                <Button onClick={createRoom} variant="contained" size="small" color="gold" style={{ borderRadius: 13, width: 140 }}> 
-                                    <Box sx={{fontSize: 20, margin: '0px 8px 0px 8px', minWidth: '50px', fontWeight: 'bold'}}>创建</Box>
-                                </Button>
-                            </ThemeProvider>
-                        </Box>
-                        
-                        <Box ml={5}>
-                            <ThemeProvider theme={theme}>
-                                <Button onClick={linkPrivateRoom} variant="contained" size="small" color='black' style={{ borderRadius: 13, width: 140 }}> 
-                                    <Box sx={{fontSize: 20, margin: '0px 8px 0px 8px', minWidth: '50px', fontWeight: 'bold'}}>取消</Box>
-                                </Button>
-                            </ThemeProvider>
+                    <Box ml={5} display='flex' alignItems='center'>
+                        <Box fontSize={18}>
+                            <input 
+                                className='create-room-name'
+                                value={name}
+                                onChange={event => setName(event.target.value)}
+                                type='text'
+                                placeholder='输入房间名' 
+                                maxLength='30' required/>
                         </Box>
                     </Box>
                 </Box>
-            </form>
+
+                <Box display='flex' alignItems='center' mt={3}>
+                    <Box fontSize={25} fontWeight='bold' ml={1} width={125}>
+                        设置密码
+                    </Box>
+
+                    <Box ml={5} display='flex' alignItems='center'>
+                        <Box fontSize={18}>
+                            <Checkbox 
+                                checked = {lock}
+                                onChange={handleLockChange}
+                                sx={{ '& .MuiSvgIcon-root': { fontSize: 40 } }}
+                                color='default'
+                            />
+                        </Box>
+                    </Box>
+                </Box>
+
+                <Box height={170} mt={3}>
+                    { lock === true ? 
+                        <Box display='flex' alignItems='center'>
+                            <Box fontSize={25} fontWeight='bold' ml={1} width={125}>
+                                密码
+                            </Box>
+
+                            <Box ml={5} display='flex' alignItems='center'>
+                                <Box fontSize={18}>
+                                    <input 
+                                        className='create-room-name'
+                                        value={password}
+                                        onChange={event => setPassword(event.target.value)}
+                                        type={ visible ? 'text' : 'password' }
+                                        placeholder='输入密码' 
+                                        maxLength='30' required/>
+                                </Box>
+                            </Box>
+
+                            <Box ml={4}>
+                                { visible ? <VisibilityIcon sx={{cursor: 'pointer'}} onClick={() => setVisible(false)}/> : <VisibilityOffIcon sx={{cursor: 'pointer'}} onClick={() => setVisible(true)}/> }
+                            </Box>
+                        </Box> : ''
+                    }
+                </Box>
+
+                <Box mt={4} ml={2} display='flex'>
+                    <Box>
+                        <ThemeProvider theme={theme}>
+                            <Button onClick={createRoom} variant="contained" size="small" color="gold" style={{ borderRadius: 13, width: 140 }}> 
+                                <Box sx={{fontSize: 20, margin: '0px 8px 0px 8px', minWidth: '50px', fontWeight: 'bold'}}>创建</Box>
+                            </Button>
+                        </ThemeProvider>
+                    </Box>
+                    
+                    <Box ml={5}>
+                        <ThemeProvider theme={theme}>
+                            <Button onClick={linkPrivateRoom} variant="contained" size="small" color='black' style={{ borderRadius: 13, width: 140 }}> 
+                                <Box sx={{fontSize: 20, margin: '0px 8px 0px 8px', minWidth: '50px', fontWeight: 'bold'}}>取消</Box>
+                            </Button>
+                        </ThemeProvider>
+                    </Box>
+                </Box>
+            </Box>
 
             <Dialog
                 fullWidth={true}
