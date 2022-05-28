1214ab65437e246633ce40ff2945a186dde79d32
jeremyytann <tanchiachun2021@gmail.com>
2022-05-29 00:16:02 +0800

    fix imports

--- Code/frontend/src/Pages/RoomViewPage.js
+++ Code/frontend/src/Pages/RoomViewPage.js
@@ -1,4 +1,4 @@
-import { Box, Button, Grid } from '@mui/material'
+import { Box, Button, Grid, Dialog, DialogTitle, DialogActions, DialogContent, DialogContentText } from '@mui/material'
 import React from 'react'
 import { useState, useEffect } from 'react'
 import { useParams, useNavigate, Navigate } from 'react-router-dom'
